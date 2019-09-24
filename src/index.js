c2e0e3a6baa5f1160a6e463050bb2ed647cd784d
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-24 16:36:29 +0300

    not yet

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./stores/reducer";
+
+const stores = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={stores}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
