786ad03b13ef659de6eb86d62f58ef7461f5f68c
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-24 22:43:31 +0300

    second

--- src/stores/actions.js
+++ src/stores/actions.js
@@ -1,7 +1,14 @@
 const ADD_AUTHOR = "ADD_AUTHOR";
+const DELETE_AUTHOR = "DELETE_AUTHOR";
 
 export const addAuthor = () => {
   return {
     type: ADD_AUTHOR
+  };
+};
+
+export const deleteAuthor = () => {
+  return {
+    type: DELETE_AUTHOR
   };
 };
--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -8,24 +8,24 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case "ADD_AUTHOR":
-      // addAuthor = () => {
-      //   const newAuthor = {
-      //     id: this.state.newAuthorId,
-      //     first_name: "Author",
-      //     last_name: "McAuthorFace",
-      //     imageUrl:
-      //       "https://www.netz.de/images/2016-11-21-firefox-focus-header-58cfedd908c25_500_300.jpg",
-      //     books: [
-      //       {
-      //         title: "Anonymous book",
-      //         color: "mysterious color"
-      //       }
-      //     ]
-      //   };
-      // };
+      const newAuthor = {
+        id: state.newAuthorId,
+        first_name: "Author",
+        last_name: "McAuthorFace",
+        imageUrl:
+          "https://www.netz.de/images/2016-11-21-firefox-focus-header-58cfedd908c25_500_300.jpg",
+        books: [
+          {
+            title: "Anonymous book",
+            color: "mysterious color"
+          }
+        ]
+      };
+
       return {
-        ...state
-        // authors: state.authors.concat(newAuthor)
+        ...state,
+        authors: state.authors.concat(newAuthor),
+        newAuthorId: state.newAuthorId + 1
       };
     default:
       return state;
